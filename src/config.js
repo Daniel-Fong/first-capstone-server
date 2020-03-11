5f28fa1d8141845dad5b87af36b52eb07513b4f1
Daniel Fong <daniel.fong.mv@gmail.com>
2020-03-11 12:46:03 -0600

    added jwt secret to config and env

--- src/config.js
+++ src/config.js
@@ -2,4 +2,5 @@ module.exports = {
     PORT: process.env.PORT || 8080,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/snapshot',
+    JWT_SECRET: process.env.JWT_SECRET || 'its a secret',
   }
