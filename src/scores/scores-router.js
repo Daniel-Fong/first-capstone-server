752850fc23d2f7ccf99051fb9e7d85b7656b854b
Daniel Fong <daniel.fong.mv@gmail.com>
2020-03-12 08:56:46 -0600

    added get scores by multi id

--- src/scores/scores-router.js
+++ src/scores/scores-router.js
@@ -3,5 +3,16 @@ const ScoresService = require('./scores-service')
 const { requireAuth } = require('../middleware/jwt-auth')
 const path = require('path')
 
-const playersRouter = express.Router()
+const scoresRouter = express.Router()
 const jsonBodyParser = express.json()
+
+scoresRouter
+    .route('/:game_id/:player_id')
+    .get(requireAuth, (req, res, next) => {
+        ScoresService.getScoresByGameIdAndPlayerId(req.app.get('db'), req.params.game_id, req.params.player_id)
+        .then(scores => {
+            return res 
+                .status(200)
+                .json(scores)
+        })
+    })
--- src/scores/scores-service.js
+++ src/scores/scores-service.js
@@ -1,3 +1,11 @@
 const scoresService = {
-    
+    getScoresByGameIdAndPlayerId(db, gameId, playerId) {
+        return db
+            .from('scores')
+            .where('score.gameid', gameId)
+            .and('score.playerid', playerId)
+            .select('*')
+    },
 }
+
+module.exports = scoresService;
